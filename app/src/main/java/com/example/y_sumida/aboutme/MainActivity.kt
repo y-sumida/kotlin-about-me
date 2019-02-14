c7341880292f90b6ca415d11596afd0fc074d3b0
Yuki Sumida <y.sumida.0201@gmail.com>
2019-02-14 23:34:43 +0900

    generate project

---
+++ app/src/main/java/com/example/y_sumida/aboutme/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.y_sumida.aboutme
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
